7fe1f62ff86b884320a5d7276db06fa7d6fe3552
Aqeel Abbas <mbook3902@gmail.com>
2025-11-03 18:42:51 +0500

    Move and update assembly code files for BCS3A
    Assembly code files were moved into the P240514_BCS3A directory for better organization. P240514_Code7.asm was updated to include PART B, which adds a function to print a triangle of stars to video memory, along with related data and main routine.

--- assignment-3/P240514_BCS3A/P240514_Code7.asm
+++ assignment-3/P240514_BCS3A/P240514_Code7.asm
@@ -1,4 +1,4 @@
-; ------------------ PART C ------------------
+; ; ------------------ PART C ------------------
 [org 0x0100]
     jmp start
 
@@ -182,3 +182,89 @@ rollNo: dw 0x0514
 size: dw 5
 rows: dw 12
 cols: dw 30
+
+
+;  ______________ PART B ___________________
+
+
+[org 0x0100]
+    jmp main
+
+print_stars:
+    push bp
+    mov bp, sp
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+    push es
+
+    mov ax, 0B800h
+    mov es, ax                 ; video memory segment
+
+    mov cx, [bp+10]            ; total rows (height)
+    xor si, si                 ; triangle row counter (0-based)
+
+outer_loop:
+    mov ax, [bp+8]             ; starting row
+    mov bx, si
+    shl bx, 1                  ; bx = si * 2 (skip one line each)
+    add ax, bx                 ; actual row = base + (si * 2)
+    mov dx, 80
+    mul dx
+    add ax, [bp+6]             ; starting column
+    shl ax, 1
+    mov di, ax
+
+    mov cx, si
+    shl cx, 1
+    inc cx                     ; number of stars = 2*si + 1
+
+inner_loop:
+    cmp cx, 0
+    je next_row
+    mov al, '*'
+    mov ah, 0x07
+    mov [es:di], ax
+    add di, 2
+    dec cx
+    jmp inner_loop
+
+next_row:
+    inc si
+    cmp si, [bp+10]
+    jl outer_loop
+
+done:
+    pop es
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    pop bp
+    ret 8
+
+;----------------------------------------
+; main program
+;----------------------------------------
+main:
+    push word [height]  ; number of rows in triangle
+    push word [rows]    ; starting row
+    push word [cols]    ; starting column
+    push word [attr]    ; attribute
+    call print_stars
+
+    mov ax, 0x4C00
+    int 0x21
+
+;----------------------------------------
+; data
+;----------------------------------------
+height: dw 5       ; triangle height
+rows:   dw 5       ; starting row
+cols:   dw 0       ; starting column
+attr:   dw 0x07    ; white-on-black
